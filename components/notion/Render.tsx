69c7631ecd0c839adabecf7f96b68f3f0c93d4d7
johyukrae <huu000008@gmail.com>
2025-03-07 16:59:08 +0900

    ㅁ

--- components/notion/Render.tsx
+++ components/notion/Render.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import dynamic from "next/dynamic";
-import { NotionRenderer } from "react-notion-x";
-import { Suspense } from "react";
+import dynamic from 'next/dynamic';
+import { NotionRenderer } from 'react-notion-x';
+import { Suspense } from 'react';
+import './Render.module.scss';
 
 interface RenderProps {
   recordMap: any;
@@ -11,7 +12,7 @@ interface RenderProps {
 
 const Collection = dynamic(
   () =>
-    import("react-notion-x/build/third-party/collection").then(
+    import('react-notion-x/build/third-party/collection').then(
       (m) => m.Collection
     ),
   { ssr: false } // SSR을 비활성화 해야 함
@@ -24,7 +25,7 @@ export const Render = ({ recordMap, rootPageId }: RenderProps) => {
         <NotionRenderer
           recordMap={recordMap}
           fullPage={true}
-          darkMode={false}
+          darkMode={true}
           rootPageId={rootPageId}
           previewImages
           components={{ Collection }}
