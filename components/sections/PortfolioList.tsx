f240e1493aaf0dbff3b18c073906b8f002132b7a
johyukrae <huu000008@gmail.com>
2025-03-17 15:46:59 +0900

    팝업 추가

--- components/sections/PortfolioList.tsx
+++ components/sections/PortfolioList.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogRoot,
+  DialogTitle,
+} from '../ui/dialog/DialogCore';
 import styles from './PortfolioList.module.scss';
 
 interface NotionPage {
@@ -14,19 +21,31 @@ interface PortfolioListProps {
 }
 
 const PortfolioList = ({ notionPages }: PortfolioListProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <section className={styles.section}>
       <h2 className={styles.title}>WORKS</h2>
       <ul className={styles.list}>
         {notionPages.map(({ id, title, summary, technology }) => (
           <li key={id} className={styles.item}>
-            <h3 className={styles.title}>{title}</h3>
-            <div className={styles.tag}>
-              {technology.map((tech) => (
-                <span key={tech}>{tech}</span>
-              ))}
-            </div>
-            <p className={styles.summary}>{summary}</p>
+            <button onClick={() => setOpen(true)}>
+              <h3 className={styles.title}>{title}</h3>
+              <p className={styles.summary}>{summary}</p>
+              <div className={styles.tag}>
+                {technology.map((tech) => (
+                  <span key={tech}>#{tech}</span>
+                ))}
+              </div>
+            </button>
+            <DialogRoot open={open} onOpenChange={setOpen}>
+              <DialogContent>
+                <DialogTitle>제목</DialogTitle>
+                <DialogDescription>
+                  모달 내용을 여기에 작성하세요.
+                </DialogDescription>
+              </DialogContent>
+            </DialogRoot>
           </li>
         ))}
       </ul>
